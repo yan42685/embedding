48ee5d57286ff99ac9dfbdcf5817a65d0b7aa395
alexyan <419725794@qq.com>
2022-03-26 10:26:32 +0800

    新建抽象模型类

--- main/transE.py
+++ main/transE.py
@@ -1,81 +1,9 @@
-from tools import generate_initial_vector, norm_l1, norm_l2, scale_to_unit_length
-import codecs
-import numpy as np
-import os
+from base_model import BaseModel
 import copy
-import time
-import random
+from tools import norm_l1, norm_l2, scale_to_unit_length
 
 
-class TransE:
-    def __init__(self, entities, relations, facts, dimension=50, learning_rate=0.01, margin=1.0, norm=1):
-        self.entities = entities
-        self.entities_set = set(entities)
-        self.relations = relations
-        self.entity_vector_dict = {}
-        self.relation_vector_dict = {}
-        self.facts = facts
-        self.dimension = dimension
-        self.learning_rate = learning_rate
-        self.margin = margin
-        self.norm = norm
-        self.total_loss = 0.0
-        # 总的样本数，用于计算平均loss
-        self.total_sample_count = 0
-        self._vector_init()
-
-    def _vector_init(self):
-        for entity in self.entities:
-            self.entity_vector_dict[entity] = generate_initial_vector(self.dimension)
-
-        for relation in self.relations:
-            relation_vector = scale_to_unit_length(generate_initial_vector(self.dimension))
-            # 不知道为什么，换成下面这行代码反而lose降低了10%左右
-            # relation_vector = norm_l2(relation_vector)
-            self.relation_vector_dict[relation] = relation_vector
-
-    def train(self, epoch_count=1, batch_size=50, data_set_name=""):
-        batch_count = int(len(self.facts) / batch_size)
-        print("batch size: ", batch_size)
-        for epoch in range(epoch_count):
-            start_time = time.time()
-            self.total_loss = 0.0
-            self.total_sample_count = 0
-            for entity in self.entity_vector_dict.keys():
-                self.entity_vector_dict[entity] = scale_to_unit_length(self.entity_vector_dict[entity])
-
-            for batch in range(batch_count):
-                positive_batch, negative_batch = self._generate_pos_neg_batch(batch_size)
-                self._update_embedding(positive_batch, negative_batch)
-            # 让学习率衰减
-            self.learning_rate = pow(0.95, epoch + 1) * self.learning_rate
-            end_time = time.time()
-            print("epoch: ", epoch + 1, "cost time: %s" % (round((end_time - start_time), 3)))
-            print("total loss: %.6f" % self.total_loss)
-            print("average loss: %.6f" % (self.total_loss / self.total_sample_count))
-            print()
-
-        self._output_result(data_set_name, batch_size)
-
-    def _generate_pos_neg_batch(self, batch_size):
-        positive_batch = random.sample(self.facts, batch_size)
-        negative_batch = []
-
-        # 随机替换正例头实体或尾实体, 得到对应的一个负例
-        for (head, relation, tail) in positive_batch:
-            random_choice = np.random.random()
-            while True:
-                if random_choice <= 0.5:
-                    head = random.choice(self.entities)
-                else:
-                    tail = random.choice(self.entities)
-                # 确保负例不在原facts中
-                if (head, relation, tail) not in self.entities_set:
-                    break
-            negative_batch.append((head, relation, tail))
-
-        return positive_batch, negative_batch
-
+class TransE(BaseModel):
     def _update_embedding(self, positive_samples, negative_samples):
         copy_entity_vector_dict = copy.copy(self.entity_vector_dict)
         copy_relation_vector_dict = copy.copy(self.relation_vector_dict)
@@ -154,26 +82,3 @@ class TransE:
 
         self.entity_vector_dict = copy_entity_vector_dict
         self.relation_vector_dict = copy_relation_vector_dict
-
-    def _output_result(self, data_set_name, batch_size):
-        data_set_name = data_set_name + "_"
-        target_dir = "target/"
-        if not os.path.exists(target_dir):
-            os.mkdir(target_dir)
-
-        with codecs.open(
-                target_dir + data_set_name + "TransE_entity_" + str(self.dimension) + "dim_batch" + str(batch_size),
-                "w") as file1:
-
-            for e in self.entity_vector_dict.keys():
-                file1.write(e + "\t")
-                file1.write(str(list(self.entity_vector_dict[e])))
-                file1.write("\n")
-
-        with codecs.open(
-                target_dir + data_set_name + "TransE_relation_" + str(self.dimension) + "dim_batch" + str(batch_size),
-                "w") as file2:
-            for r in self.relation_vector_dict.keys():
-                file2.write(r + "\t")
-                file2.write(str(list(self.relation_vector_dict[r])))
-                file2.write("\n")
