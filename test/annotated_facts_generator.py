894ee9e5fe1319e732a16dac1e88d92ec018712a
alexyan <419725794@qq.com>
2022-03-27 20:46:29 +0800

    去除测试模式代码

--- test/annotated_facts_generator.py
+++ test/annotated_facts_generator.py
@@ -14,32 +14,31 @@ def main():
 
 class AnnotatedFactsGenerator:
     def __init__(self):
-        self.charset = "utf-8"
-        self.yago4_annotated_facts_path = Path.cwd().parent.joinpath("main/target/yago4-wd-annotated-facts.ntx")
-        self.yago4_facts_path = Path.cwd().parent.joinpath("main/target/yago4-wd-facts.ntx")
+        self.CHARSET = "utf-8"
+        # 过滤出在头实体位置出现次数 >= threshold的实体所在在的四元组
+        self.FILTER_THRESHOLD = 10
+        self.input_path = Path.cwd().parent.joinpath("main/target/yago4-wd-annotated-facts.ntx")
 
-        self.test_annotated_quads_path = Path.cwd().parent.joinpath("main/target/test_annotated_quads.txt")
-        self.yago4_annotated_quads_path = Path.cwd().parent.joinpath("main/target/yago4_annotated_quads.txt")
-        self.yago4_mixed_quads_path = Path.cwd().parent.joinpath("main/target/yago4_mixed_quads.txt")
+        self.output_path = Path.cwd().parent.joinpath("main/target/yago4_annotated_quads.txt")
 
     @time_it
     def run(self):
-        with codecs.open(self.yago4_annotated_facts_path, encoding=self.charset) as file:
-            text = file.readlines()
-        extracted_quads = self._extract_quads(text)
-        filtered_quads = self._filter_quads(extracted_quads, 10)
+        extracted_quads = self._extract_quads()
+        filtered_quads = self._filter_quads(extracted_quads)
         self._output_quads(filtered_quads)
 
     @time_it
-    def _extract_quads(self, text):
-        element_pattern = re.compile(r'(?<=<)[^<>]+(?=>)')
+    def _extract_quads(self):
+        with codecs.open(self.input_path, encoding=self.CHARSET) as file:
+            text = file.readlines()
+        elements_pattern = re.compile(r'(?<=<)[^<>]+(?=>)')
         date_pattern = re.compile(r'(?<=")\d{4}.*(?=")')
         quads = []
         initial_count = 0
         final_count = 0
         for line in text:
             initial_count += 1
-            elements = element_pattern.findall(line)
+            elements = elements_pattern.findall(line)
             if len(elements) == 5 and elements[3].endswith("startDate"):
                 final_count += 1
                 date = date_pattern.findall(line)[0]
@@ -51,19 +50,15 @@ class AnnotatedFactsGenerator:
         return quads
 
     @time_it
-    def _filter_quads(self, quads, threshold):
-        """
-        :param quads:
-        :param threshold: 过滤出在头实体位置出现次数 >= threshold的实体所在在的四元组
-        """
+    def _filter_quads(self, quads):
         entity_dict = collections.defaultdict(int)
         entity_set = set()
         for (a, b, c, d) in quads:
             entity_dict[a] += 1
             entity_dict[c] += 1
-            if entity_dict[a] >= threshold:
+            if entity_dict[a] >= self.FILTER_THRESHOLD:
                 entity_set.add(a)
-            if entity_dict[c] >= threshold:
+            if entity_dict[c] >= self.FILTER_THRESHOLD:
                 entity_set.add(c)
 
         print("filtered quad: %d" % len(entity_set))
@@ -71,8 +66,8 @@ class AnnotatedFactsGenerator:
 
     @time_it
     def _output_quads(self, quads):
-        output_path = self.yago4_annotated_quads_path
-        with codecs.open(output_path, "w", encoding=self.charset) as output_file:
+        output_path = self.output_path
+        with codecs.open(output_path, "w", encoding=self.CHARSET) as output_file:
             for quad in quads:
                 new_line = "\t".join(quad)
                 output_file.write(new_line + "\n")
