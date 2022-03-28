c7c252f36c80242787326312ffad610f8f28345e
alexyan <419725794@qq.com>
2022-03-28 20:37:53 +0800

    读取KG

--- main/model/KG.py
+++ main/model/KG.py
@@ -1,30 +1,40 @@
 import codecs
+from pathlib import Path
 
 
 class KG:
+    _ENTITY_FILENAME = "entity.txt"
+    _RELATION_FILENAME = "relation.txt"
+    _TRAIN_FILENAME = "train.txt"
+    _VALIDATION_FILENAME = "validation.txt"
+    _TEST__FILENAME = "test.txt"
 
-    def __init__(self, file_path, sep="\t"):
+    def __init__(self, directory: Path, sep="\t"):
+        self.directory = directory
         self.sep = sep
         self.entities = []
         self.relations = []
-        self.facts = []
-        self._load_kg(file_path)
+        self.train_quads = []
+        self.validation_quads = []
+        self.test_quads = []
+        self.all_quads = []
+        self._load_kg(directory)
 
-    def _load_kg(self, file_path):
-        entity_set = set()
-        relation_set = set()
-        facts = []
-        with codecs.open(file_path, "r") as file:
-            lines = file.readlines()
-            for line in lines:
-                quad = tuple(line.split(self.sep))
-                entity_set.add(quad[0])
-                entity_set.add(quad[2])
-                relation_set.add(quad[1])
-                facts.append(quad)
+    def _load_kg(self, directory):
+        for line in self._read_lines(self._ENTITY_FILENAME):
+            self.entities.append(line.strip("\t"))
+        for line in self._read_lines(self._RELATION_FILENAME):
+            self.relations.append(line.strip("\t"))
+        for line in self._read_lines(self._TRAIN_FILENAME):
+            self.train_quads.append(tuple(line.split(self.sep)))
+        for line in self._read_lines(self._VALIDATION_FILENAME):
+            self.validation_quads.append(tuple(line.split(self.sep)))
+        for line in self._read_lines(self._TEST__FILENAME):
+            self.test_quads.append(tuple(line.split(self.sep)))
+        self.all_quads = self.train_quads + self.validation_quads + self.test_quads
 
-        self.entities = list(entity_set)
-        self.relations = list(relation_set)
-        self.facts = facts
-
-
+    def _read_lines(self, filename):
+        path = self.directory.joinpath(filename)
+        with codecs.open(path, "r") as file:
+            result = file.readlines()
+        return result
