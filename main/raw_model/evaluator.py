fb01e8ac71e3c62fb236cba6c6bb5282db3da028
alexyan <419725794@qq.com>
2022-03-30 19:12:03 +0800

    解决性能问题

--- main/raw_model/evaluator.py
+++ main/raw_model/evaluator.py
@@ -1,4 +1,6 @@
 from tools import norm_l1, norm_l2, time_it
+import time
+import scipy.stats as ss
 
 
 # 评估模型训练的效果
@@ -14,6 +16,8 @@ class Evaluator:
     def evaluate(self):
         self._calculate_mean_rank_and_hits10()
 
+    # 大概三分钟
+    @time_it
     def _calculate_mean_rank_and_hits10(self):
         h_correct_rank_sum = 0
         t_correct_rank_sum = 0
@@ -33,7 +37,6 @@ class Evaluator:
 
         print("raw mean rank: %.4f, raw hits10: %.4f%%" % (raw_mean_rank, raw_hits10))
 
-    @time_it
     # 对于某一个测试quad，计算每个实体在头实体预测和尾实体预测中的排名
     def _calculate_h_t_predict_ranks(self, quad):
         h = self.entity_embeddings[quad[0]]
@@ -41,6 +44,7 @@ class Evaluator:
         t = self.entity_embeddings[quad[2]]
         h_predict_distances = []
         t_predict_distances = []
+
         for i in range(self.entity_count):
             h_predict_distance = self.entity_embeddings[i] + r - t
             t_predict_distance = h + r - self.entity_embeddings[i]
@@ -52,7 +56,7 @@ class Evaluator:
                 t_predict_distances.append(norm_l2(t_predict_distance))
 
         # 得到每个实体预测距离的排名，排名从1开始，比如[5,2,4,1] -> [4,2,3,1]
-        h_predict_ranks = [sorted(h_predict_distances).index(x) + 1 for x in h_predict_distances]
-        t_predict_ranks = [sorted(t_predict_distances).index(x) + 1 for x in t_predict_distances]
+        h_predict_ranks = ss.rankdata(h_predict_distances)
+        t_predict_ranks = ss.rankdata(t_predict_distances)
 
         return h_predict_ranks, t_predict_ranks
